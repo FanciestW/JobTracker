06cebb292587bc2293cb6a0b4b42ff3510d0a499
William Lin <wlin26@yahoo.com>
2019-10-28 11:24:08 -0400

    Added background color and grid

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Fab, Paper, List, ListItem } from '@material-ui/core';
+import { Fab, Paper, Grid, List, ListItem } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import './Dashboard.scss';
 
@@ -7,39 +7,45 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <h1>Dashboard</h1>
-        <Paper>
-          <List>
-            <ListItem>
-              Test1
-            </ListItem>
-            <ListItem>
-              Test2
-            </ListItem>
-            <ListItem>
-              Test3
-            </ListItem>
-            <ListItem>
-              Test4
-            </ListItem>
-          </List>
-        </Paper>
-        <Paper>
-          <List>
-            <ListItem>
-              Test1
-            </ListItem>
-            <ListItem>
-              Test2
-            </ListItem>
-            <ListItem>
-              Test3
-            </ListItem>
-            <ListItem>
-              Test4
-            </ListItem>
-          </List>
-        </Paper>
+        <Grid container direction="column" spacing={2} style={{height: '100%', width: '100%', margin: '0'}}>
+          <h1>Dashboard</h1>
+          <Grid item>
+            <Paper>
+              <List>
+                <ListItem>
+                  Test1
+                </ListItem>
+                <ListItem>
+                  Test2
+                </ListItem>
+                <ListItem>
+                  Test3
+                </ListItem>
+                <ListItem>
+                  Test4
+                </ListItem>
+              </List>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper>
+              <List>
+                <ListItem>
+                  Test1
+                </ListItem>
+                <ListItem>
+                  Test2
+                </ListItem>
+                <ListItem>
+                  Test3
+                </ListItem>
+                <ListItem>
+                  Test4
+                </ListItem>
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
         <Fab color="primary" aria-label="add" className="add-fab">
           <AddIcon />
         </Fab>
