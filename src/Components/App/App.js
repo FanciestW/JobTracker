a792ab8064f8bd0120ac598c32f7f0b041513ba0
William Lin <wlin26@yahoo.com>
2019-10-28 21:05:13 -0400

    Added material ui themes and Cssbaseline

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import Navbar from '../Navbar/Navbar';
 import Dashboard from '../Dashboard/Dashboard';
 import './App.scss';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="app-content">
-        <Dashboard />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <div className="app-content">
+          <Dashboard />
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
