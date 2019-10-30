52effb6369cf9f0b23a7cd7ff6db238ba61defd4
William Lin <wlin26@yahoo.com>
2019-10-30 09:53:57 -0400

    Added a list item component for job applications

---
+++ src/Components/JobApplicationListItem/JobApplicationListItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class JobApplicationListItem extends Component {
+  render() {
+    return (
+      <p>List Item</p>
+    );
+  }
+}
+
+export default JobApplicationListItem;
