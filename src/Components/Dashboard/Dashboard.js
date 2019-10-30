11f33276946773162c0e925dc2b675ac4f2e616f
William Lin <wlin26@yahoo.com>
2019-10-30 12:31:31 -0400

    Working on JobApplicationListItem component

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
-import { Fab, Paper, Grid, List, ListItem } from '@material-ui/core';
+import { Fab, Paper, Grid, List, ListItem, Divider } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import JobApplicationListItem from '../JobApplicationListItem/JobApplicationListItem';
 import './Dashboard.scss';
 
 class Dashboard extends Component {
   render() {
+    const jobs = [
+      {
+        jobTitle: 'Software Engineer Intern',
+        jobCompany: 'Microsoft',
+        jobStatus: 'Applied',
+      },
+      {
+        jobTitle: 'Software Engineer New Grad',
+        jobCompany: 'Facebook',
+        jobStatus: 'Interviewing',
+      }
+    ];
     return (
       <div className="dashboard">
-        <Grid container direction="column" spacing={2} style={{height: '100%', width: '100%', margin: '0'}}>
-          <h1>Upcoming Interviews</h1>
+        <Grid container direction="column" spacing={2} style={{ height: '100%', width: '100%', margin: '0' }}>
+          <h1>Recent Job Applications</h1>
           <Grid item>
             <Paper>
               <List>
-                <ListItem>
-                  Test1
-                </ListItem>
-                <ListItem>
-                  Test2
-                </ListItem>
-                <ListItem>
-                  Test3
-                </ListItem>
-                <ListItem>
-                  Test4
-                </ListItem>
+                {jobs.map((jobObj, index) => (
+                  <div key={index}>
+                    <ListItem>
+                      <JobApplicationListItem jobTitle={jobObj.jobTitle} jobCompany={jobObj.jobCompany} jobStatus={jobObj.jobStatus} />
+                    </ListItem>
+                    {index + 1 < jobs.length ? <Divider /> : null}
+                  </div>
+                ))}
               </List>
             </Paper>
           </Grid>
-          <h1>Interested Jobs</h1>
+          <h1>Upcoming Interviews</h1>
           <Grid item>
             <Paper>
               <List>
--- src/Components/JobApplicationListItem/JobApplicationListItem.js
+++ src/Components/JobApplicationListItem/JobApplicationListItem.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { Grid, Checkbox, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
 
 class JobApplicationListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <p>List Item</p>
+      <Grid container direction="row" justify={this.props.justify} alignItems="center">
+        <Grid item xs={1} alignItems="center">
+          <Checkbox style={{padding: '9px 0'}} inputProps={{'aria-label': 'checkbox'}} />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body1" noWrap>{this.props.jobTitle}</Typography>
+        </Grid>
+        <Grid item xs>
+          <Typography variant="body1" noWrap>{this.props.jobCompany}</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="body1" noWrap>{this.props.jobStatus}</Typography>
+        </Grid>
+      </Grid>
     );
   }
 }
+
+JobApplicationListItem.propTypes = {
+  jobTitle: PropTypes.string.isRequired,
+  jobCompany: PropTypes.string.isRequired,
+  jobStatus: PropTypes.string.isRequired,
+  justify: PropTypes.string,
+};
+
+JobApplicationListItem.defaultProps = {
+  justify: 'left'
+};
 
 export default JobApplicationListItem;
