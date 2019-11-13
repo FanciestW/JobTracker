3f1d7cf6c37b32300957cef6f9480d4651e4f769
William Lin <wlin26@yahoo.com>
2019-11-12 22:00:44 -0500

    Added protected routes and implemented private routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { MuiThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Dashboard from '../Dashboard/Dashboard';
 import ErrorPage from '../ErrorPage/ErrorPage';
+import Login from '../SignInUp/Login/Login';
+import SignUp from '../SignInUp/SignUp/SignUp';
 import './App.scss';
+import AuthedRoute from '../AuthedRoute/AuthedRoute';
 
 let theme = createMuiTheme({
   palette: {
@@ -14,25 +17,41 @@ let theme = createMuiTheme({
 });
 theme = responsiveFontSizes(theme);
 
+const fakeAuth = {
+  isAuthenticated: true,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
+
 function App() {
   const [ authed, setAuthed ] = useState(false);
 
   return (
-    <div className="App">
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <Navbar />
-          <div className="app-content">
-            <Switch>
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path="/" component={Dashboard} />
-              <Route component={ErrorPage} />
-            </Switch>
-          </div>
-        </Router>
-      </MuiThemeProvider>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <PrivateRoute path="/dashboard" component={AuthedRoute} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 }
 
--- src/Components/AuthedRoute/AuthedRoute.js
+++ src/Components/AuthedRoute/AuthedRoute.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import Route from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
+import Dashboard from '../Dashboard/Dashboard';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import './AuthedRoute.scss';
 
 class AuthedRoute extends Component {
   render() {
     return (
-      <h1>AuthedRoute</h1>
+      <div className='app'>
+        <Router>
+          <Navbar />
+          <div className="app-content">
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/" component={Dashboard} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
