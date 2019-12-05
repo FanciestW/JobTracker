8154aa2d4b3a3866aa6fb32b90c75d4114d62853
William Lin <wlin26@yahoo.com>
2019-12-05 17:45:28 -0500

    Added job listings

--- src/Components/AuthedApp/AuthedApp.js
+++ src/Components/AuthedApp/AuthedApp.js
@@ -4,6 +4,7 @@ import Navbar from '../Navbar/Navbar';
 import Dashboard from '../Dashboard/Dashboard';
 import JobApplicationsView from '../JobApplicationsView/JobApplicationsView';
 import JobInterviewsView from '../JobInterviewsView/JobInterviewsView';
+import JobListingView from '../JobListingsView/JobListingsView';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import './AuthedApp.scss';
 
@@ -18,6 +19,7 @@ class AuthedApp extends Component {
               <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/jobapps" component={JobApplicationsView} />
               <Route exact path="/interviews" component={JobInterviewsView} />
+              <Route exact path="/jobs" component={JobListingView} />
               <Route exact path="/" component={Dashboard} />
               <Route component={ErrorPage} />
             </Switch>
---
+++ src/Components/JobListingsView/JobListingsView.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Grid, Paper, Table, TableRow, TableHead, TableCell, TableBody } from '@material-ui/core';
+import Axios from 'axios';
+
+class JobListingsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+    };
+    this.getJobListings = this.getJobListings.bind(this);
+  }
+
+  componentDidMount() {
+    this.getJobListings();
+  }
+
+  async getJobListings(description = undefined, location = undefined) {
+    const jobRes = await Axios.get('/api/jobs', { params: { description, location } });
+    const { jobs } = jobRes.data;
+    this.setState({ jobs });
+  }
+
+  render() {
+    const jobListingItems = this.state.jobs.map((job, index) => (
+      <TableRow key={index}>
+        <TableCell component="th" scope="row">
+          {job.title}
+        </TableCell>
+        <TableCell align="right">{job.company}</TableCell>
+        <TableCell align="right">{job.location}</TableCell>
+        <TableCell align="right">{job.type}</TableCell>
+      </TableRow>
+    ));
+    return (
+      <div>
+        <Grid container direction="column" spacing={2} style={{ height: '100%', width: '100%', margin: '0' }}>
+          <Grid item xs>
+            <Paper style={{ height: '100%', width: '100%', overflow: 'auto' }}>
+              <Table style={{ minWidth: '100%' }}>
+                <TableHead>
+                  <TableRow style={{ minWidth: '100%' }}>
+                    <TableCell>Job Title</TableCell>
+                    <TableCell align="right">Company</TableCell>
+                    <TableCell align="right">Location</TableCell>
+                    <TableCell align="right">Type</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {jobListingItems}
+                </TableBody>
+              </Table>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default JobListingsView;
